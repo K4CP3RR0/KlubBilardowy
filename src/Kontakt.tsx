f5a58025e41384bbb271f26845ce2d20006a8745
Kacper78PL <64462953+Kacper78PL@users.noreply.github.com>
2026-03-06 16:10:21 +0100

    Add More Files

---
+++ src/Kontakt.tsx
@@ -0,0 +1,10 @@
+function Kontakt() {
+
+    return (
+      <div className="container">
+            <h1 className="text-white text-center">Kontakt</h1>
+        </div>
+  )
+}
+
+export default Kontakt
---
+++ src/PanelPracownika.tsx
@@ -0,0 +1,10 @@
+function PanelPracownika() {
+
+    return (
+      <div className="container">
+            <h1 className="text-white text-center">Panel pracownika</h1>
+        </div>
+  )
+}
+
+export default PanelPracownika
