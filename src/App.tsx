b97899e5944bdf14946f418f35950130d90abafd
realfaux <o.chomski@proton.me>
2026-03-06 17:09:53 +0100

    Utworzenie podstrony cennika oraz utworzenie testowej ścieżki do buttona.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 //import { supabase } from '../utils/supabase'
+import { Link } from 'react-router-dom';
+
+
 function App() {
+
 
     return (
       <div className="container">
             <h1 className="text-white text-center">Klub bilardowy</h1>
+          <Link to="/kontakt">
+              <button>Kontakt</button>
+          </Link>
         </div>
   )
 }
