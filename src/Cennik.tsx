a692aebf7e1c139a3b5263e4cd203e509385cf8f
realfaux <o.chomski@proton.me>
2026-03-06 17:09:53 +0100

    Utworzenie podstrony cennika oraz utworzenie testowej ścieżki do buttona.

---
+++ src/Cennik.tsx
@@ -0,0 +1,11 @@
+function Cennik() {
+
+    return (
+        <div className="container">
+            <h1 className="text-white text-center">Cennik</h1>
+
+        </div>
+    )
+}
+
+export default Cennik
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-
 import App from './App.tsx'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Kontakt from "./Kontakt.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" element={<App />} />
+              <Route path="/kontakt" element={<Kontakt/>} />
+          </Routes>
+      </BrowserRouter>
   </StrictMode>,
 )
