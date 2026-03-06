7c26fbf4b71279f61f83ab2dbc7eaf93a9a7d807
realfaux <o.chomski@proton.me>
2026-03-06 18:30:30 +0100

    Test

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 //import { supabase } from '../utils/supabase'
+//import { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 
 
 function App() {
+/*    const [, setTodos] = useState([])
+    useEffect(() => {
+        function getTodos() {
+            //const { data: todos } = await supabase.from('todos').select()
 
+            if (todos.length > 1) {
+                setTodos(todos)
+            }
+        }
 
+        getTodos()
+    }, [])*/
     return (
       <div className="container">
             <h1 className="text-white text-center">Klub bilardowy</h1>
           <Link to="/kontakt">
-              <button>Kontakt</button>
+              <button className="btn btn-primary">Kontakt</button>
           </Link>
           <Link to="/cennik">
               <button>Cennik</button>
@@ -21,7 +32,17 @@ function App() {
               <button>Rezerwacje</button>
           </Link>
         </div>
+
   )
 }
+
+
+
+
+
+
+
+
+
 
 export default App
---
+++ src/PanelUzytkownika.tsx
@@ -0,0 +1,10 @@
+function PanelUzytkownika() {
+
+    return (
+        <div className="container">
+            <h1 className="text-white text-center">Panel Uzytkownika</h1>
+        </div>
+    )
+}
+
+export default PanelUzytkownika
