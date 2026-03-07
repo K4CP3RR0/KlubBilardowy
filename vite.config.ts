0c0246432d5a0f1bb2a075fce4c1542fd52f4a3e
Kacper78PL <64462953+Kacper78PL@users.noreply.github.com>
2026-03-07 01:23:45 +0100

    1

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: '0.0.0.0',
+    port: 4173,
+    allowedHosts: [
+      'bilard.kacpercichorski.tech',
+      '192.168.1.194',
+      'localhost'
+    ]
+  }
 })
