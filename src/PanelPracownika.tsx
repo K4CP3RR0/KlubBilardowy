1fcb35d0afc9e865c4cd70a8bbabc5ef11c4122c
Kacper78PL <64462953+Kacper78PL@users.noreply.github.com>
2026-03-07 17:47:08 +0100

    Panel Pracownika Update

--- src/PanelPracownika.tsx
+++ src/PanelPracownika.tsx
@@ -1,20 +1,27 @@
+import {Link} from "react-router-dom";
+
 function PanelPracownika() {
 
     return (
         <div className="container text-center">
             <div className="row px-2 column-gap-3">
-        <div className="card mb-3 w-25">
+
+
+                <div className="card mb-3 w-25">
+                    <Link to="/">
             <div className="row g-0">
                 <div className="col-md-4">
                     <img src="./assets/images/poolv1.jpg" className="img-fluid rounded-start" alt="Brak"/>
                 </div>
                 <div className="col-md-8">
                     <div className="card-body">
-                        <h5 className="card-title">Table 1</h5>
+                       <h5 className="card-title">Table 1</h5>
                     </div>
                 </div>
             </div>
-        </div>
+                </Link>
+            </div>
+
             <div className="card mb-3 w-25">
             <div className="row g-0">
                 <div className="col-md-4">
