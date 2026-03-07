3741c6be8a80140a336cda6f5982a8317790f0a0
Kacper78PL <64462953+Kacper78PL@users.noreply.github.com>
2026-03-07 15:15:07 +0100

    Panel Pracownika Update

--- src/PanelPracownika.tsx
+++ src/PanelPracownika.tsx
@@ -1,10 +1,125 @@
 function PanelPracownika() {
 
     return (
-      <div className="container">
-            <h1 className="text-white text-center">Panel pracownika</h1>
+        <div className="container text-center">
+            <div className="row px-2 column-gap-3">
+        <div className="card mb-3 w-25">
+            <div className="row g-0">
+                <div className="col-md-4">
+                    <img src="./assets/images/poolv1.jpg" className="img-fluid rounded-start" alt="Brak"/>
+                </div>
+                <div className="col-md-8">
+                    <div className="card-body">
+                        <h5 className="card-title">Table 1</h5>
+                    </div>
+                </div>
+            </div>
         </div>
-  )
+            <div className="card mb-3 w-25">
+            <div className="row g-0">
+                <div className="col-md-4">
+                    <img src="./assets/images/poolv1.jpg" className="img-fluid rounded-start" alt="..."/>
+                </div>
+                <div className="col-md-8">
+                    <div className="card-body">
+                        <h5 className="card-title">Table 2</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+            <div className="card mb-3 w-25">
+            <div className="row g-0">
+                <div className="col-md-4">
+                    <img src="./assets/images/poolv1.jpg" className="img-fluid rounded-start" alt="..."/>
+                </div>
+                <div className="col-md-8">
+                    <div className="card-body">
+                        <h5 className="card-title">Table 3</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+            < br/>
+            <div className="row px-2 column-gap-3">
+        <div className="card mb-3 w-25">
+            <div className="row g-0">
+                <div className="col-md-4">
+                    <img src="./assets/images/poolv1.jpg" className="img-fluid rounded-start" alt="Brak"/>
+                </div>
+                <div className="col-md-8">
+                    <div className="card-body">
+                        <h5 className="card-title">Table 4</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+            <div className="card mb-3 w-25">
+            <div className="row g-0">
+                <div className="col-md-4">
+                    <img src="./assets/images/poolv1.jpg" className="img-fluid rounded-start" alt="..."/>
+                </div>
+                <div className="col-md-8">
+                    <div className="card-body">
+                        <h5 className="card-title">Table 5</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+            <div className="card mb-3 w-25">
+            <div className="row g-0">
+                <div className="col-md-4">
+                    <img src="./assets/images/poolv1.jpg" className="img-fluid rounded-start" alt="..."/>
+                </div>
+                <div className="col-md-8">
+                    <div className="card-body">
+                        <h5 className="card-title">Table 6</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+            <br/>
+            <div className="row px-2 column-gap-3">
+        <div className="card mb-3 w-25 ">
+            <div className="row g-0 ">
+                <div className="col-md-4">
+                    <img src="./assets/images/poolv1.jpg" className="img-fluid rounded-start" alt="Brak"/>
+                </div>
+                <div className="col-md-8">
+                    <div className="card-body">
+                        <h5 className="card-title">Table 7</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+            <div className="card mb-3 w-25">
+            <div className="row g-0">
+                <div className="col-md-4">
+                    <img src="./assets/images/poolv1.jpg" className="img-fluid rounded-start" alt="..."/>
+                </div>
+                <div className="col-md-8">
+                    <div className="card-body">
+                        <h5 className="card-title">Table 8</h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+            <div className="card mb-3 w-25">
+            <div className="row g-0">
+                <div className="col-md-4">
+                    <img src="./assets/images/poolv1.jpg" className="img-fluid rounded-start" alt="..."/>
+                </div>
+                <div className="col-md-8">
+                    <div className="card-body">
+                        <h5 className="card-title">Table 9 </h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </div>
+        </div>
+    )
 }
 
 export default PanelPracownika
