4a9bb5ddf623803323dc6999a925e530bbb18d69
Kacper78PL <64462953+Kacper78PL@users.noreply.github.com>
2026-03-10 13:03:39 +0100

    Update Panel Pracownika

--- src/PanelPracownika.tsx
+++ src/PanelPracownika.tsx
@@ -1,131 +1,23 @@
-import {Link} from "react-router-dom";
+// import {Link} from "react-router-dom";
 import {useTitle} from "./hooks/useTitle.tsx";
+import TableCard from "./components/TableCard.tsx";
 function PanelPracownika() {
     useTitle("Panel Pracownika");
     return (
 
-        <div className="container text-center">
-            <div className="row px-2 column-gap-3">
-
-
-                <div className="card mb-3 w-25">
-                    <Link to="/">
-            <div className="row g-0">
-                <div className="col-md-4">
-                    <img src="./assets/images/poolv1.jpg" className="img-fluid rounded-start" alt="Brak"/>
-                </div>
-                <div className="col-md-8">
-                    <div className="card-body">
-                       <h5 className="card-title">Table 1</h5>
-                    </div>
-                </div>
-            </div>
-                </Link>
-            </div>
-
-            <div className="card mb-3 w-25">
-            <div className="row g-0">
-                <div className="col-md-4">
-                    <img src="./assets/images/poolv1.jpg" className="img-fluid rounded-start" alt="..."/>
-                </div>
-                <div className="col-md-8">
-                    <div className="card-body">
-                        <h5 className="card-title">Table 2</h5>
-                    </div>
-                </div>
-            </div>
-        </div>
-            <div className="card mb-3 w-25">
-            <div className="row g-0">
-                <div className="col-md-4">
-                    <img src="./assets/images/poolv1.jpg" className="img-fluid rounded-start" alt="..."/>
-                </div>
-                <div className="col-md-8">
-                    <div className="card-body">
-                        <h5 className="card-title">Table 3</h5>
-                    </div>
-                </div>
-            </div>
-        </div>
-        </div>
-            < br/>
-            <div className="row px-2 column-gap-3">
-        <div className="card mb-3 w-25">
-            <div className="row g-0">
-                <div className="col-md-4">
-                    <img src="./assets/images/poolv1.jpg" className="img-fluid rounded-start" alt="Brak"/>
-                </div>
-                <div className="col-md-8">
-                    <div className="card-body">
-                        <h5 className="card-title">Table 4</h5>
-                    </div>
-                </div>
-            </div>
-        </div>
-            <div className="card mb-3 w-25">
-            <div className="row g-0">
-                <div className="col-md-4">
-                    <img src="./assets/images/poolv1.jpg" className="img-fluid rounded-start" alt="..."/>
-                </div>
-                <div className="col-md-8">
-                    <div className="card-body">
-                        <h5 className="card-title">Table 5</h5>
-                    </div>
-                </div>
-            </div>
-        </div>
-            <div className="card mb-3 w-25">
-            <div className="row g-0">
-                <div className="col-md-4">
-                    <img src="./assets/images/poolv1.jpg" className="img-fluid rounded-start" alt="..."/>
-                </div>
-                <div className="col-md-8">
-                    <div className="card-body">
-                        <h5 className="card-title">Table 6</h5>
-                    </div>
-                </div>
-            </div>
-        </div>
-        </div>
-            <br/>
-            <div className="row px-2 column-gap-3">
-        <div className="card mb-3 w-25 ">
-            <div className="row g-0 ">
-                <div className="col-md-4">
-                    <img src="./assets/images/poolv1.jpg" className="img-fluid rounded-start" alt="Brak"/>
-                </div>
-                <div className="col-md-8">
-                    <div className="card-body">
-                        <h5 className="card-title">Table 7</h5>
-                    </div>
-                </div>
-            </div>
-        </div>
-            <div className="card mb-3 w-25">
-            <div className="row g-0">
-                <div className="col-md-4">
-                    <img src="./assets/images/poolv1.jpg" className="img-fluid rounded-start" alt="..."/>
-                </div>
-                <div className="col-md-8">
-                    <div className="card-body">
-                        <h5 className="card-title">Table 8</h5>
-                    </div>
-                </div>
-            </div>
-        </div>
-            <div className="card mb-3 w-25">
-            <div className="row g-0">
-                <div className="col-md-4">
-                    <img src="./assets/images/poolv1.jpg" className="img-fluid rounded-start" alt="..."/>
-                </div>
-                <div className="col-md-8">
-                    <div className="card-body">
-                        <h5 className="card-title">Table 9 </h5>
-                    </div>
-                </div>
-            </div>
-        </div>
-        </div>
+        <div className="flex flex-col m-2">
+            <TableCard id="1" image="/favicon.svg" type="Table" status="Dostępny" />
+            <TableCard id="2" image="/favicon.svg" type="Table" status="Dostępny" />
+            <TableCard id="3" image="/favicon.svg" type="Table" status="Dostępny" />
+            <TableCard id="4" image="/favicon.svg" type="Table" status="Dostępny" />
+            <TableCard id="5" image="/favicon.svg" type="Table" status="Dostępny" />
+            <TableCard id="6" image="/favicon.svg" type="Table" status="Dostępny" />
+            <TableCard id="7" image="/favicon.svg" type="Table" status="Dostępny" />
+            <TableCard id="8" image="/favicon.svg" type="Table" status="Dostępny" />
+            <TableCard id="9" image="/favicon.svg" type="Table" status="Dostępny" />
+            <TableCard id="10" image="/favicon.svg" type="Table" status="Dostępny" />
+            <TableCard id="11" image="/favicon.svg" type="Dart" status="Dostępny" />
+            <TableCard id="12" image="/favicon.svg" type="Dart" status="Dostępny" />
         </div>
     )
 }
