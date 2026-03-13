94c33abee5ca5c7fcb8c902c645ac1029a5b7907
Kacper78PL <64462953+Kacper78PL@users.noreply.github.com>
2026-03-13 16:35:28 +0100

    Update Panel Pracownika

--- src/PanelPracownika.tsx
+++ src/PanelPracownika.tsx
@@ -4,21 +4,29 @@ import TableCard from "./components/TableCard.tsx";
 function PanelPracownika() {
     useTitle("Panel Pracownika");
     return (
-
-        <div className="flex flex-col m-2">
-            <TableCard id="1" image="/favicon.svg" type="Table" status="Dostępny" />
-            <TableCard id="2" image="/favicon.svg" type="Table" status="Dostępny" />
-            <TableCard id="3" image="/favicon.svg" type="Table" status="Dostępny" />
-            <TableCard id="4" image="/favicon.svg" type="Table" status="Dostępny" />
-            <TableCard id="5" image="/favicon.svg" type="Table" status="Dostępny" />
-            <TableCard id="6" image="/favicon.svg" type="Table" status="Dostępny" />
-            <TableCard id="7" image="/favicon.svg" type="Table" status="Dostępny" />
-            <TableCard id="8" image="/favicon.svg" type="Table" status="Dostępny" />
-            <TableCard id="9" image="/favicon.svg" type="Table" status="Dostępny" />
-            <TableCard id="10" image="/favicon.svg" type="Table" status="Dostępny" />
-            <TableCard id="11" image="/favicon.svg" type="Dart" status="Dostępny" />
-            <TableCard id="12" image="/favicon.svg" type="Dart" status="Dostępny" />
+        <div>
+            <div className="flex flex-col3 m-2 gap-2">
+                <TableCard id="1" image="/pool-table.svg" type="Table" status="Dostępny" />
+                <TableCard id="2" image="/pool-table.svg" type="Table" status="Dostępny" />
+                <TableCard id="3" image="/pool-table.svg" type="Table" status="Dostępny" />
+            </div>
+            <div className="flex flex-col3 m-5 gap-2">
+                <TableCard id="4" image="/pool-table.svg" type="Table" status="Dostępny" />
+                <TableCard id="5" image="/pool-table.svg" type="Table" status="Dostępny" />
+                <TableCard id="6" image="/pool-table.svg" type="Table" status="Dostępny" />
+            </div>
+            <div className="flex flex-col3 m-5 gap-2">
+                <TableCard id="7" image="/pool-table.svg" type="Table" status="Dostępny" />
+                <TableCard id="8" image="/pool-table.svg" type="Table" status="Dostępny" />
+                <TableCard id="9" image="/pool-table.svg" type="Table" status="Dostępny" />
+            </div>
+            <div className="flex flex-col3 m-5 gap-2">
+                <TableCard id="10" image="/pool-table.svg" type="Table" status="Dostępny" />
+                <TableCard id="11" image="/dart.png" type="Dart" status="Dostępny" />
+                <TableCard id="12" image="/dart.png" type="Dart" status="Dostępny" />
+            </div>
         </div>
+
     )
 }
 
--- src/components/TableCard.tsx
+++ src/components/TableCard.tsx
@@ -1,15 +1,23 @@
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-expect-error
 const TableCard = ({ id, type, status, image }) => (
-  <div className="bg-gray-900 border-2 border-green-500 rounded-xl overflow-hidden cursor-pointer hover:scale-95 transition-transform">
+  <div className="bg-gray-900 border-2 border-green-500 rounded-xl overflow-hidden cursor-pointer hover:scale-95 transition-transform w-full">
+      <button popoverTarget={id} popoverTargetAction="show" className="w-full">
     <img src={image} alt={type} className="h-32 w-50% object-cover" />
     <div className="p-4">
       <div className="flex justify-between items-center">
-        <h3 className="text-white font-bold">{type} #{id}</h3>
+        <h3 className="text-white font-bold">{type} {id}</h3>
         <span className="px-2 py-1 bg-green-900 text-green-400 text-xs rounded-full">{status}</span>
       </div>
-      <p className="text-gray-400 text-sm mt-2">Kliknij, aby sprawdzić rezerwacje</p>
+       <p className="text-white font-bold">Kliknij aby sprawdzić rezerwacje</p>
     </div>
-  </div>
+      </button>
+      <div popover="auto" id={id} className="fixed inset-0 m-auto ml-10 flex-col items-center justify-center w-full h-70 rounded-md bg-[#03346E] box-content">
+          <h2 className="text-center text-white font-bold mb-4">{type} #{id}</h2>
+          <button popoverTarget={id} popoverTargetAction="hide" className="rounded-lg bg-green-500 text-white p-3 hover:bg-green-600 transition-colors">Wróć do panelu</button>
+      </div>
+    </div>
+
+
 );
 export default TableCard;
