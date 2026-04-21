ce134e9bfcd34f5fa9cf8c5d9a1415131445d8bb
K4CP3RR0 <kacek.cichy@gmail.com>
2026-04-21 20:36:20 +0200

    Test deploy

--- src/Rezerwacje.tsx
+++ src/Rezerwacje.tsx
@@ -7,6 +7,7 @@ function Rezerwacje() {
       <div className="">
             <div className="m-6">
                 <h1 className="text-6xl montserrat-extrabold text-center text-[#f5f5dc]">Strona w budowie</h1>
+                <h1 className="text-6xl montserrat-extrabold text-center text-[#f5f5dc]">Rezerwacje</h1>
             </div>
           <Footer/>
         </div>
